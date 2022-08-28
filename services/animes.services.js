0a33a99fc6b2f04a8e19b6ce55162326a34ed32f
dhainara <daynaralinnyker@gmail.com>
2022-08-28 16:55:49 -0300

    Implementação de erros nas rotas

--- services/animes.services.js
+++ services/animes.services.js
@@ -3,28 +3,31 @@ import CharacterEntity from "../entities/characters.entity.js"
 import AnimeEntity from "../entities/animes.entity.js"
 
 function findAllAnimes() {
-    return animes
+    console.log('findAllAnimes executado.')
+    if (animes.length == 0) {
+        return ('Não há animes registrados. Vá para http://localhost:3000/create-anime e cadastre um anime agora!')
+    } else {
+        return animes
+    }
 }
 
 function findAnimeById(id) {
-    let idAnime
-    let chosenAnime
-    animes.map((anime) => {
-        if (anime.id === idAnime){
-        chosenAnime = anime
-        }
-    })
+    console.log('findAnimesById executado.')
+
+    const anime = animes.find((anime) =>anime.id ==id)
 
     let count = animes.length
 
-    if (chosenAnime) { //se o anime com o id escolhido for encontrado
-        return chosenAnime
+    if (anime) { //se o anime com o id escolhido for encontrado
+        return anime
     } else {
-        return (`Nenhum anime com esse id foi encontrado.\nHá ${count} id's registrados.\n Verifique a lista e e tente novamente com um id existente!`)
+        return (`Nenhum anime com esse id foi encontrado.\nHá ${count} id's registrados. Verifique a lista e tente novamente com um id existente!`)
     }
 }
 
 function createAnime(anime) {
+    console.log('createAnime executado.')
+
     console.log(anime)
     const newAnime = new AnimeEntity(anime)
     newAnime.validateAnime()
@@ -49,6 +52,8 @@ function createAnime(anime) {
 }
 
 function updateAnime(anime) {
+    console.log('updateAnime executado.')
+
     const updateAnime = new AnimeEntity(anime)
     updateAnime.validateAnime()
 
@@ -75,8 +80,18 @@ function updateAnime(anime) {
 }
 
 function deleteAnime(id) {
+    console.log('deleteAnime executado.')
+
     const animeIndex = animes.findIndex((element => element.id == id))
     animes.splice(animeIndex, 1)
+
+    if (animeIndex) {
+        return ('Anime deletado!')
+    }else if (animes.length = true) {
+        return ('Não há nenhum anime registrado na lista para ser deletado!')
+
+    }
+    
 }
 
 export const animesService = {
